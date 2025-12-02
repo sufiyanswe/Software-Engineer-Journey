6937037f64e343ad34523e9a773016d324cb4797
sufiyanswe <sufiyanswe@gmail.com>
2025-12-02 20:35:32 +0530

    feat(array): add CountElements program to count array size

--- Month_01_Nov_2025_CoreJava/Day07/CountElements.java
+++ Month_01_Nov_2025_CoreJava/Day07/CountElements.java
@@ -1,4 +1,33 @@
 package Day07;
 
+/**
+ * Problem: Count Elements in an Array
+ * Author: Zayn
+ * Description:
+ *   Given an integer array, count and return the number of elements in it.
+ *   This demonstrates basic array traversal using an enhanced for-loop.
+ */
+
 public class CountElements {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25, 30};
+        System.out.println("Array has " + countElements(arr) + " elements");
+    }
+
+    /**
+     * Counts how many elements the array contains.
+     *
+     * @param arr input integer array (may be empty)
+     * @return the total number of elements
+     */
+    static int countElements(int[] arr) {
+        int count = 0;
+
+        for (int num : arr) {
+            count++;
+        }
+
+        return count;
+    }
 }
